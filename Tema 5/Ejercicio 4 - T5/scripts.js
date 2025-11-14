92b5f13e289bae5fccc6aaea9d43a9e80d7312b8
MiguelMatMar <miguelmm0005@gmail.com>
2025-11-14 09:33:13 +0100

    Mas cambios

--- Tema 5/Ejercicio 4 - T5/scripts.js
+++ Tema 5/Ejercicio 4 - T5/scripts.js
@@ -2,21 +2,24 @@ class parImpar{
     constructor(num){
         this.numeroUsr = parseInt(num);
     }
-    error(){
-        console.log("Dato Erroneo");
+    error(numero){
+        if(typeof(numero) !== "number"){
+            return console.log("Dato Erroneo");
+        }
     }
     resuelve(){
         this.numeroUsr % 2 === 0 ? alert("Par") : alert("Impar");
     }
-    es(n){
-        let esParImpar = (n) => (n % 2 === 0) ? 'par' : 'impar';
-        esParImpar(this.numeroUsr)
+    es(numero){
+        return numero % 2 === 0 ? 'par' : 'impar';
+        
     }
     muestraRandom(){
         let aleatorio;
         for(let i = 0; i< 10; i++){
-            aleatorio = Math.random()*10000 +1;
-            document.write(`El numero ${aleatorio} es ${this.es(aleatorio)}`);
+            aleatorio = parseInt(Math.random()*10000 +1);
+            document.write(`<br> El numero ${aleatorio} es ${this.es(aleatorio)} <br>`);
+            console.log(this.es(aleatorio));
         }
     }
 }
