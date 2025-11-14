1b290b450741fc5b5947b6c73d27e79253df07a9
MiguelMatMar <miguelmm0005@gmail.com>
2025-11-14 09:16:10 +0100

    Nuevos Cambios

--- Tema 5/Ejercicio 4 - T5/scripts.js
+++ Tema 5/Ejercicio 4 - T5/scripts.js
@@ -1,6 +1,22 @@
 class parImpar{
     constructor(num){
-        this.numeroUsr = num;
+        this.numeroUsr = parseInt(num);
     }
-    
+    error(){
+        console.log("Dato Erroneo");
+    }
+    resuelve(){
+        this.numeroUsr % 2 === 0 ? alert("Par") : alert("Impar");
+    }
+    es(n){
+        let esParImpar = (n) => (n % 2 === 0) ? 'par' : 'impar';
+        esParImpar(this.numeroUsr)
+    }
+    muestraRandom(){
+        let aleatorio;
+        for(let i = 0; i< 10; i++){
+            aleatorio = Math.random()*10000 +1;
+            document.write(`El numero ${aleatorio} es ${this.es(aleatorio)}`);
+        }
+    }
 }
