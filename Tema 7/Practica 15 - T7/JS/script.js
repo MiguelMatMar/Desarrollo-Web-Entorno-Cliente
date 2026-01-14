d31c3b8410f8c9d70077c40e984572aed4ba753c
MiguelMatMar <miguelmm0005@gmail.com>
2026-01-14 21:13:26 +0100

    Faltaba la operacion del boton

--- Tema 7/Practica 15 - T7/JS/script.js
+++ Tema 7/Practica 15 - T7/JS/script.js
@@ -1,11 +1,16 @@
 let contenedor = document.getElementsByTagName('div')[0];
 let creado = false;
+let boton = document.createElement('button');
 
 contenedor.addEventListener('scroll', () => {
     if (contenedor.scrollTop + contenedor.clientHeight >= contenedor.scrollHeight - 1 && !creado) { // Si ha llegado al fondo y el boton no se ha creado
-        let boton = document.createElement('button');
         boton.textContent = "Aceptar";
         document.body.appendChild(boton);
         creado = true;
     }
 });
+
+boton.addEventListener('click', ()=>{
+    contenedor.style.display = "none";
+    boton.style.display = "none";
+})
