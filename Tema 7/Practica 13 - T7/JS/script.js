ffbf99c6da901945579e145259330820fa772057
MiguelMatMar <miguelmm0005@gmail.com>
2026-01-14 19:33:08 +0100

    Era con alt

--- Tema 7/Practica 13 - T7/JS/script.js
+++ Tema 7/Practica 13 - T7/JS/script.js
@@ -1,7 +1,7 @@
 let fondo = document.getElementById('cuerpoDocumento');
 
 document.addEventListener('keydown', (e) => {
-    if (e.key === "F12" && e.shiftKey) {
+    if (e.key === "F12" && e.altKey) {
         fondo.style.backgroundImage = 'url("https://picsum.photos/800/600")';
         document.getElementsByTagName('h1')[0].style.display = "none";
     }
